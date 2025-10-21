37fcedf93ee5f92f0b74f0ac1a66f80c0c5e10a6
rameessiyad <rameesmsiyadofficial786@gmail.com>
2025-10-21 15:23:57 +0530

    bug fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,30 @@ closeIcon.addEventListener("click", () => {
 
   document.body.classList.remove("overflow-hidden");
 });
+
+// header animation on scroll
+const header = document.querySelector("header");
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > lastScrollY) {
+    // Scrolling down → hide header
+    header.style.transform = "translateY(-100%)";
+  } else {
+    // Scrolling up → show header
+    header.style.transform = "translateY(0)";
+  }
+  lastScrollY = window.scrollY;
+});
+
+// go to top button
+window.addEventListener("scroll", () => {
+  // Show after scrolling 200vh
+  if (window.scrollY > window.innerHeight * 2) {
+    goTopBtn.classList.remove("hidden");
+    goTopBtn.classList.add("block");
+  } else {
+    goTopBtn.classList.remove("block");
+    goTopBtn.classList.add("hidden");
+  }
+});
